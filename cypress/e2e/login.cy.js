914240e381e4d1b7fb10edf3058341f44a69b4c0
anthonycdp <anthonycoelho.dp@hotmail.com>
2025-06-24 21:58:06 -0300

    Adding baseUrl

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,7 +12,7 @@ describe('ORANGE HRM TEST', () => {
   }
 
   it('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
@@ -20,7 +20,7 @@ describe('ORANGE HRM TEST', () => {
     cy.get(selectorList.dashboardGrid)
   })
     it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField). type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
